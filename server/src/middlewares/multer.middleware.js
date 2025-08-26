552aaba8d0cc69c4737baf45c424011fbc9f2211
Ankan Samanta <ankan.samanta8017@gmail.com>
2025-08-26 16:34:52 +0530

    multer file updated

--- server/src/middlewares/multer.middleware.js
+++ server/src/middlewares/multer.middleware.js
@@ -2,13 +2,11 @@ import multer from "multer";
 
 const storage = multer.diskStorage({
   destination: function (req, file, cb) {
-    cb(null, "./public/temp");
+    cb(null, "/tmp"); // temporary folder on Vercel
   },
   filename: function (req, file, cb) {
     cb(null, Date.now() + "-" + file.originalname);
   },
 });
 
-export const upload = multer({
-  storage,
-});
+export const upload = multer({ storage });
